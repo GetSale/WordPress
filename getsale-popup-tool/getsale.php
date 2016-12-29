44ea58afe628a4f3b3c2917d09fcc816fb38d2a4
gambit <chernozor@yandex.ru>
2016-12-29 13:11:41 +0300

    ver 1.0.2 notice fixed

--- getsale-popup-tool/getsale.php
+++ getsale-popup-tool/getsale.php
@@ -59,6 +59,164 @@ function getsale_add_filter($product_id) {
     return $product_id;
 }
 
+function getsale_script_cookie() {
+    if (isset($_COOKIE['getsale_add'])) {
+        add_action('wp_enqueue_scripts', 'getsale_scripts_add');
+        setcookie('getsale_add', '', time() + 3600 * 24 * 100, COOKIEPATH, COOKIE_DOMAIN, false);
+    }
+
+    if (isset($_COOKIE['getsale_del'])) {
+        add_action('wp_enqueue_scripts', 'getsale_scripts_del');
+        setcookie('getsale_del', '', time() + 3600 * 24 * 100, COOKIEPATH, COOKIE_DOMAIN, false);
+    }
+};
+
+add_action('init', 'getsale_script_cookie');
+
+add_action('admin_enqueue_scripts', 'getsale_script_translate');
+
+function getsale_script_translate() {
+    wp_enqueue_script( 'getsale-main-script', dirname( plugin_basename(__FILE__) ) . 'js/admin.js');
+    wp_localize_script( 'getsale-main-script', 'gs', array(
+        'authorization' => __( 'Authorization', 'getsale-popup-tool' ),
+        'enter_value' => __( 'Please, enter your Email and API Key from your GetSale account', 'getsale-popup-tool'),
+        'registration' => __( 'If you don’t have GetSale account, you can register it <a href=\'https://getsale.io\'>here</a>', 'getsale-popup-tool'),
+        'support' => __( 'Contact Us: <a href=\'mailto:support@getsale.io\'>support@getsale.io</a>', 'getsale-popup-tool' ),
+        'getsale_ver' => '1.0.2',
+        'congrats' => __( 'Congratulations! Your website is successfully linked to your <a href=\'https://getsale.io\'>GetSale account</a>', 'getsale-popup-tool'),
+        'widgets_create' => __( 'You can start creating widgets for your website using your <a href=\'https://getsale.io\'>GetSale account</a>!', 'getsale-popup-tool'),
+        'api_key_success' => __( 'API Key is correct', 'getsale-popup-tool' ),
+        'email_success' => __( 'Email is correct', 'getsale-popup-tool' ),
+        'error403' => __( 'Attention! API Key is invalid. Please, check and enter API Key once again', 'getsale-popup-tool'),
+        'error404' => __( 'Attention! This Email isn’t registered on <a href=\'https://getsale.io\'>GetSale</a>', 'getsale-popup-tool'),
+        'error500' => __( 'Attention! This website is already in use on <a href=\'https://getsale.io\'>GetSale</a>', 'getsale-popup-tool'),
+        'error0' => __( 'You don\'t have Curl support in your PHP!', 'getsale-popup-tool'),
+        'desc' => __( 'powerful cutting edge tool to create widgets and popups for your website!', 'getsale-popup-tool'),
+        'description' => __( 'GetSale is a powerful tool for creating all types of widgets for your website. You can increase your sales dramatically creating special offer, callback widgets, coupons blasts and many more. Create, Show and Sell - this is our goal!', 'getsale-popup-tool'),
+        'getsale_name' => __( 'GetSale Popup Tool', 'getsale-popup-tool'),
+        'path' => plugins_url('ok.png', __FILE__),
+    ));
+}
+
+function getsale_scripts_method() {
+    $options = get_option('getsale_option_name');
+    if ($options['getsale_project_id'] !== '') {
+        wp_register_script('getsale_handle', plugins_url('js/main.js', __FILE__), array('jquery'));
+
+        $datatoBePassed = array('project_id' => $options['getsale_project_id']);
+        wp_localize_script('getsale_handle', 'getsale_vars', $datatoBePassed);
+
+        wp_enqueue_script('getsale_handle');
+    }
+}
+
+function getsale_scripts_add() {
+    function getsale_echo_before_text() {
+        echo '<div id=\'before_install\' style=\'display:none;\'>' . __('GetSale Popup Tool has been successfully installed', 'getsale-popup-tool') . '<br/>' . __('To get started, you must enter Email and API Key, from from your <a href=\'https://getsale.io\'>GetSale account</a>', 'getsale-popup-tool') . '</div>
+<div class="wrap" id="after_install" style="display:none;">
+<p><b>' . __('GetSale Popup Tool', 'getsale-popup-tool') . '</b> &mdash; ' . __('professional tool for creating popup windows', 'getsale-popup-tool') . '</p>
+<p>' . __('GetSale is a powerful tool for creating all types of widgets for your website. You can increase your sales dramatically creating special offer, callback widgets, coupons blasts and many more. Create, Show and Sell - this is our goal!', 'getsale-popup-tool') . '</p>
+</div>
+</div>
+<script type=\'text/javascript\'>
+    window.onload = function () {
+        if (document.location.search == \'?option=com_installer&view=install\') {
+            document.getElementById(\'before_install\').style.display = \'block\';
+        } else document.getElementById(\'after_install\').style.display = \'block\';
+    }
+</script>';
+    }
+
+    $options = get_option('getsale_option_name');
+    if ($options['getsale_project_id'] !== '') {
+        wp_register_script('getsale_add', plugins_url('js/add.js', __FILE__), array('jquery'));
+        wp_enqueue_script('getsale_add');
+    }
+}
+
+function getsale_reg($regDomain, $email, $key, $url) {
+    $domain = $regDomain;
+    if (($domain == '') OR ($email == '') OR ($key == '') OR ($url == '')) {
+        return;
+    }
+
+    if (!function_exists('curl_init')) {
+        $json_result = '';
+        $json_result->status = 'error';
+        $json_result->code = 0;
+        $json_result->message = 'No Curl!';
+        return $json_result;
+    };
+
+    $ch = curl_init();
+    $jsondata = json_encode(array(
+        'email' => trim($email),
+        'key' => $key,
+        'url' => $url,
+        'cms' => 'wordpress'
+    ));
+
+    $options = array(CURLOPT_HTTPHEADER => array('Content-Type:application/json', 'Accept: application/json'), CURLOPT_URL => $domain . '/api/registration.json', CURLOPT_POST => 1, CURLOPT_POSTFIELDS => $jsondata, CURLOPT_RETURNTRANSFER => true);
+
+    curl_setopt_array($ch, $options);
+    $json_result = json_decode(curl_exec($ch));
+    curl_close($ch);
+    if (isset($json_result->status)) {
+        if (($json_result->status == 'OK') && (isset($json_result->payload))) {
+        } elseif ($json_result->status = 'error') {
+        }
+    }
+    return $json_result;
+}
+
+
+function getsale_scripts_del() {
+    $options = get_option('getsale_option_name');
+    if ($options['getsale_project_id'] !== '') {
+        wp_register_script('getsale_add', plugins_url('js/del.js', __FILE__), array('jquery'));
+        wp_enqueue_script('getsale_add');
+    }
+}
+
+function getsale_set_default_code() {
+    $options = get_option('getsale_option_name');
+    if (is_bool($options)) {
+        $options = array();
+        $options['getsale_email'] = '';
+        $options['getsale_api_key'] = '';
+        $options['getsale_project_id'] = '';
+        $options['getsale_reg_error'] = '';
+        update_option('getsale_option_name', $options);
+    }
+}
+
+register_activation_hook(__FILE__, 'getsale_admin_actions');
+register_uninstall_hook( __FILE__, 'getsale_plugin_uninstall');
+
+function getsale_plugin_uninstall(){
+    delete_option('getsale_option_name');
+}
+
+add_action('admin_menu', 'getsale_admin_actions');
+
+function getsale_admin_actions() {
+    if (current_user_can('manage_options')) {
+        if (function_exists('add_meta_box')) {
+            add_menu_page('GetSale Settings', 'GetSale', 'manage_options', 'getsale_settings', 'getsale_custom_menu_page', plugin_dir_url(__FILE__) . '/img/logo.png', 100);
+        }
+    }
+}
+
+function getsale_custom_menu_page() {
+    $getsale_settings_page = new getsaleSettingsPage();
+    if (!isset($getsale_settings_page)) {
+        wp_die(__('Plugin GetSale has been installed incorrectly.'));
+    }
+    if (function_exists('add_plugins_page')) {
+        add_plugins_page('GetSale Settings', 'GetSale', 'manage_options', 'getsale_settings', array(&$getsale_settings_page, 'getsale_create_admin_page'));
+    }
+}
+
 $options = get_option('getsale_option_name');
 
 if (is_admin()) {
@@ -103,47 +261,3 @@ if (is_admin()) {
     }
 }
 
-function getsale_script_cookie() {
-    if (isset($_COOKIE['getsale_add'])) {
-        add_action('wp_enqueue_scripts', 'getsale_scripts_add');
-        setcookie('getsale_add', '', time() + 3600 * 24 * 100, COOKIEPATH, COOKIE_DOMAIN, false);
-    }
-
-    if (isset($_COOKIE['getsale_del'])) {
-        add_action('wp_enqueue_scripts', 'getsale_scripts_del');
-        setcookie('getsale_del', '', time() + 3600 * 24 * 100, COOKIEPATH, COOKIE_DOMAIN, false);
-    }
-};
-
-add_action('init', 'getsale_script_cookie');
-
-add_action('admin_enqueue_scripts', 'getsale_script_translate');
-
-function getsale_script_translate() {
-    wp_enqueue_script( 'getsale-main-script', dirname( plugin_basename(__FILE__) ) . 'js/admin.js');
-    wp_localize_script( 'getsale-main-script', 'gs', array(
-        'authorization' => __( 'Authorization', 'getsale-popup-tool' ),
-        'enter_value' => __( 'Please, enter your Email and API Key from your GetSale account', 'getsale-popup-tool'),
-        'registration' => __( 'If you don’t have GetSale account, you can register it <a href=\'https://getsale.io\'>here</a>', 'getsale-popup-tool'),
-        'support' => __( 'Contact Us: <a href=\'mailto:support@getsale.io\'>support@getsale.io</a>', 'getsale-popup-tool' ),
-        'getsale_ver' => '1.0.2',
-        'congrats' => __( 'Congratulations! Your website is successfully linked to your <a href=\'https://getsale.io\'>GetSale account</a>', 'getsale-popup-tool'),
-        'widgets_create' => __( 'You can start creating widgets for your website using your <a href=\'https://getsale.io\'>GetSale account</a>!', 'getsale-popup-tool'),
-        'api_key_success' => __( 'API Key is correct', 'getsale-popup-tool' ),
-        'email_success' => __( 'Email is correct', 'getsale-popup-tool' ),
-        'error403' => __( 'Attention! API Key is invalid. Please, check and enter API Key once again', 'getsale-popup-tool'),
-        'error404' => __( 'Attention! This Email isn’t registered on <a href=\'https://getsale.io\'>GetSale</a>', 'getsale-popup-tool'),
-        'error500' => __( 'Attention! This website is already in use on <a href=\'https://getsale.io\'>GetSale</a>', 'getsale-popup-tool'),
-        'error0' => __( 'You don\'t have Curl support in your PHP!', 'getsale-popup-tool'),
-        'desc' => __( 'powerful cutting edge tool to create widgets and popups for your website!', 'getsale-popup-tool'),
-        'description' => __( 'GetSale is a powerful tool for creating all types of widgets for your website. You can increase your sales dramatically creating special offer, callback widgets, coupons blasts and many more. Create, Show and Sell - this is our goal!', 'getsale-popup-tool'),
-        'getsale_name' => __( 'GetSale Popup Tool', 'getsale-popup-tool'),
-        'path' => plugins_url('ok.png', __FILE__),
-    ));
-}
-
-register_uninstall_hook( __FILE__, 'getsale_plugin_uninstall');
-
-function getsale_plugin_uninstall(){
-    delete_option('getsale_option_name');
-}
